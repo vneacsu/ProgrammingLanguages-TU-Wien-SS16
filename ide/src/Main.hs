fc9aa7f85b577f25fb53e3432cc3cff18bcbde2c
Valentin - Mihai Neacsu <neacsustoi_92@yahoo.com>
2016-08-20 23:30:44 +0200

    assg3 - add <file path> command line argument

--- ide/src/Main.hs
+++ ide/src/Main.hs
@@ -1,12 +1,22 @@
+import System.Exit
+import System.Environment
 import UI.NCurses
 
 main :: IO ()
-main = runCurses $ do
+main = do
+    args <- getArgs
+    case length args of
+        0 -> die "USAGE: ide <file path>"
+        _ -> openInEditor $ head args
+
+
+openInEditor :: String -> IO ()
+openInEditor file = runCurses $ do
     setEcho False
     w <- defaultWindow
     updateWindow w $ do
         moveCursor 1 10
-        drawString "Hello world!"
+        drawString $ "Editing file: " ++ file
         moveCursor 3 10
         drawString "(press q to quit)"
         moveCursor 0 0
